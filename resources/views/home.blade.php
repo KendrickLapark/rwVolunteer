cdca6ac4a5255e4caa96e45fa567e50780a20fbc
José Expósito Ávila <EXAVJO@magtel.es>
2023-02-21 15:13:37 +0100

    Modificando diseño home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,50 @@
 @endsection
 
 @section('content')
-    <img src="<?php echo asset('images/mainSlider.jpg'); ?>" alt="slider" id="sliderHome" />
+
+    <div class="slider">
+        <div class="slides">
+            <input type="radio" name="radio-btn" id="radio1">
+            <input type="radio" name="radio-btn" id="radio2">
+            <input type="radio" name="radio-btn" id="radio3">
+            <input type="radio" name="radio-btn" id="radio4">
+
+            <div class="slide first">
+                <img src="images/sliderhome/1.png" alt="">
+            </div>
+            <div class="slide">
+                <img src="images/sliderhome/2.png" alt="">
+            </div>
+            <div class="slide">
+                <img src="images/sliderhome/3.png" alt="">
+            </div>
+            <div class="slide">
+                <img src="images/sliderhome/4.png" alt="">
+            </div>
+
+            <div class="navigation-auto">
+                <div class="auto-btn1"></div>
+                <div class="auto-btn2"></div>
+                <div class="auto-btn3"></div>
+                <div class="auto-btn4"></div>
+            </div>        
+
+            
+
+        </div>
+
+        <div class="navigation-manual">
+            <label for="radio1" class="manual-btn"></label>
+            <label for="radio2" class="manual-btn"></label>
+            <label for="radio3" class="manual-btn"></label>
+            <label for="radio4" class="manual-btn"></label>
+        </div>
+        
+    </div>
+
+    {{-- slider inicial --}}
+
+    {{-- <img src="<?php echo asset('images/mainSlider.jpg'); ?>" alt="slider" id="sliderHome" /> --}}
     <div id="contentDiv">
         <div class="rowDiv">           
             <div class="eachCell">
