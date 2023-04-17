2c074db43ff080e9dc293f61048a57311f4fbe08
José Expósito Ávila <EXAVJO@magtel.es>
2023-04-17 18:05:09 +0200

    reconstruyendo toolbar-toggle-a11y

--- resources/views/common/base.blade.php
+++ resources/views/common/base.blade.php
@@ -6,10 +6,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"></script>
     <link rel="stylesheet" href="<?php echo asset('css/reset.css'); ?>" type="text/css">
     <link rel="stylesheet" href="<?php echo asset('css/base.css'); ?>" type="text/css">
     <link rel="stylesheet" href="<?php echo asset('css/toolbar.toggle.css'); ?>" type="text/css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" 
+    integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
     <script type="text/javascript" src="{{ URL::asset('js/a11y.js') }}"></script>
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
     <link rel="icon" type="image/png" href="<?php echo asset('images/favicon.png'); ?>" />
--- resources/views/dashboard/partials/toolbar_toggle.blade.php
+++ resources/views/dashboard/partials/toolbar_toggle.blade.php
@@ -1,8 +1,9 @@
 <div class="toolbar">
     <input type="checkbox" name="" id="check">
         <div class="toggle-overlay">
+            
             <label for="check" id="overlay1">
-                <span class=icon-overlay> </span>
+                <span class=icon-overlay> <i class="fa-solid fa-wheelchair" id="icon-toolbar" ></i> </span>
             </label>
         </div>
 
@@ -16,37 +17,37 @@
 
                     <li class="toolbar-item" id="ti1"> 
                         <a href="#/">
-                            <span class="toolbar-text">  Aumentar texto </span>
+                            <i class="fa-solid fa-magnifying-glass-plus" color="black"></i> <span class="toolbar-text"> Aumentar texto </span>              
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti2"> 
                         <a href="#/">
-                            <span class="toolbar-text">  Disminuir texto </span>
+                            <i class="fa-solid fa-magnifying-glass-minus"></i> <span class="toolbar-text">  Disminuir texto </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti3">
                         <a href="#/">
-                            <span class="toolbar-text" >  Escala de grises </span>
+                            <i class="fa-solid fa-barcode"></i><span class="toolbar-text" >  Escala de grises </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti4">
                         <a href="#/">
-                            <span class="toolbar-text">  Alto contraste </span>
+                            <i class="fa-solid fa-circle-half-stroke"></i> <span class="toolbar-text">  Alto contraste </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti5">
                         <a href="#/">
-                            <span class="toolbar-text">  Contraste negativo </span>
+                            <i class="fa-solid fa-eye"></i> <span class="toolbar-text">  Contraste negativo </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti6">
                         <a href="#/">
-                            <span class="toolbar-text">  Fondo claro </span>
+                            <i class="fa-regular fa-lightbulb"></i> <span class="toolbar-text">  Fondo claro </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti7">
                         <a href="#/">
-                            <span class="toolbar-text">  Restablecer </span>
+                            <i class="fa-solid fa-arrow-rotate-right"></i> <span class="toolbar-text">  Restablecer </span>
                         </a>
                     </li>      
                 </ul>
