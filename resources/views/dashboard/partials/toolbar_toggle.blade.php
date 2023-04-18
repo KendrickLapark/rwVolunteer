729f4164418db52eaa826c666c2936bd13227416
José Expósito Ávila <EXAVJO@magtel.es>
2023-04-18 10:14:00 +0200

    retocando toolbar-toggle-a11y

--- resources/views/dashboard/partials/toolbar_toggle.blade.php
+++ resources/views/dashboard/partials/toolbar_toggle.blade.php
@@ -16,37 +16,37 @@
                 <ul class="toolbar-items">
 
                     <li class="toolbar-item" id="ti1"> 
-                        <a href="#/">
+                        <a href="#/" style="text-decoration: none">
                             <i class="fa-solid fa-magnifying-glass-plus" color="black"></i> <span class="toolbar-text"> Aumentar texto </span>              
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti2"> 
-                        <a href="#/">
+                        <a href="#/" style="text-decoration: none">
                             <i class="fa-solid fa-magnifying-glass-minus"></i> <span class="toolbar-text">  Disminuir texto </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti3">
-                        <a href="#/">
+                        <a href="#/" style="text-decoration: none">
                             <i class="fa-solid fa-barcode"></i><span class="toolbar-text" >  Escala de grises </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti4">
-                        <a href="#/">
+                        <a href="#/" style="text-decoration: none">
                             <i class="fa-solid fa-circle-half-stroke"></i> <span class="toolbar-text">  Alto contraste </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti5">
-                        <a href="#/">
+                        <a href="#/" style="text-decoration: none">
                             <i class="fa-solid fa-eye"></i> <span class="toolbar-text">  Contraste negativo </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti6">
-                        <a href="#/">
+                        <a href="#/" style="text-decoration: none">
                             <i class="fa-regular fa-lightbulb"></i> <span class="toolbar-text">  Fondo claro </span>
                         </a>
                     </li>
                     <li class="toolbar-item" id="ti7">
-                        <a href="#/">
+                        <a href="#/" style="text-decoration: none">
                             <i class="fa-solid fa-arrow-rotate-right"></i> <span class="toolbar-text">  Restablecer </span>
                         </a>
                     </li>      
