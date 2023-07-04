4dfaba6ca956ee81c33e091527f6dcb36e2914f7
José Expósito Ávila <EXAVJO@magtel.es>
2023-07-04 10:51:26 +0200

    modificando authcontroller create

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -28,7 +28,7 @@ class NotifyController extends Controller
     public static function notifyTrigger()
     {
         $isNotCompleted=Document::select('volunteer_id', DB::raw('SUM(isContactModelVol)') , DB::raw('SUM(isInscripModelVol)')) 
-                                ->join('Volunteer', 'documents.volunteer_id','=','Volunteer.id')
+                                ->join('volunteer', 'documents.volunteer_id','=','Volunteer.id')
                                 ->groupBy('volunteer_id')
                                 ->where('isAdminVol','=',0)
                                 ->where('isRegisterComplete','=',0)
